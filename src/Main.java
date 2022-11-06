47e863370b755b84ab4b4b04945515563f335094
Home <ildar86@live.ru>
2022-11-06 19:59:40 +0500

    ДЗ 3, выполнено первое задание

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int exampleOne = 356;
+        System.out.println("Значение переменной exampleOne с типом int равно " + exampleOne );
+        byte exampleTwo = 121;
+        System.out.println("Значение переменной exampleTwo с типом byte равно " + exampleTwo);
+        short exampleThree = 22222;
+        System.out.println("Значение переменной exampleThree с типом short равно " + exampleThree);
+        long exampleFour = 825735121;
+        System.out.println("Значение переменной exampleFour с типом long равно " + exampleFour );
+        float f = 1.53f;
+        System.out.println("Значение переменной f с типом float равно " + f);
+        double d = 35.6;
+        System.out.println("Значение переменной d с типом double равно " + d);
+
+
+    }
+}
