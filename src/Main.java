44995e3229158018516fb50c45aaf1694f09773b
Home <ildar86@live.ru>
2022-11-06 21:16:37 +0500

    ДЗ 3, выполнено второе задание

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,33 @@ public class Main {
         System.out.println("Значение переменной exampleTwo с типом byte равно " + exampleTwo);
         short exampleThree = 22222;
         System.out.println("Значение переменной exampleThree с типом short равно " + exampleThree);
-        long exampleFour = 825735121;
+        long exampleFour = 9825735121L;
         System.out.println("Значение переменной exampleFour с типом long равно " + exampleFour );
         float f = 1.53f;
         System.out.println("Значение переменной f с типом float равно " + f);
         double d = 35.6;
         System.out.println("Значение переменной d с типом double равно " + d);
+
+        double dbl = 27.12;
+        System.out.println(dbl);
+        long exampleLong = 987678965549L;
+        System.out.println(exampleLong);
+        char c = 50;
+        System.out.println(c);
+        short sh = 786;
+        System.out.println(sh);
+        boolean isAdult = false;
+        System.out.println(isAdult);
+        int i = 569;
+        System.out.println(i);
+        float fl = -159.00f;
+        System.out.println(fl);
+        int iTwo = 27897;
+        System.out.println(iTwo);
+        byte b = 67;
+        System.out.println(b);
+
+
 
 
     }
